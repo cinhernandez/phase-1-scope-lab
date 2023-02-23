dcde9c42c1b149fac8d7f9ea8e006e2338721cb8
Cindy Hernandez <cindyh@mail.com>
2023-02-23 10:46:44 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+
+function upperCaseCustomerName(){
+   customerName = customerName.toLocaleUpperCase();
+}
+console.log(customerName);
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    
+
+}
+console.log(bestCustomer);
+setBestCustomer();
+
+function overwriteBestCustomer (){
+    bestCustomer='maybe bob';
+}
+
+console.log(bestCustomer);
+overwriteBestCustomer();
+
+
+
+const leastFavoriteCustomer= 'Sasha';
+
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = 'Larry'
+   
+
+}
+changeLeastFavoriteCustomer();
